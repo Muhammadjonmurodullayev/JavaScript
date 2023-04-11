d9bcc0728616bf9e369d26a77a66ed5cea5efd91
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-04-11 10:43:30 +0500

    Dars-2

---
+++ Dars-2/index.js
@@ -0,0 +1,41 @@
+let login = 12
+let pw = 12
+if(login&&pw){
+    console.log('Rigth!');
+}else{
+    console.log('You dont know "Ecmascript"');
+}
+// // =====================================================================================================================
+// ========================================================================================================================
+//                               2)Misol
+// let son = 0
+// if(son==0){
+//     console.log(0);
+// }
+// else if  (son>1){
+// console.log(1);
+// }else{
+// console.log(-1);
+// }
+// =========================================================================================================================
+//                                3)Misol
+// let a = 1, b = 2
+// if (a + b < 4) {
+//     console.log('Below');
+// } else {
+//     console.log('Over');
+// }
+// a+b<4?console.log('Below'):console.log('Over');//javobi
+// ============================================================================================================================
+// 4)Misol
+// let login='director'
+// if(login=='eployee'){
+//     console.log('hello');
+// }else if (login=='director'){
+//     console.log('Greetings');
+// }else if(login==""){
+//     console.log('no login');
+// }else{
+//     console.log('');
+// }
+// login=='eployee'?console.log('Hello'):login=='director'?console.log('Greetings'):login==""?console.log('no login'):console.log("");//javobi
