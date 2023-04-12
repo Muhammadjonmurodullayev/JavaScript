aa3e90b2b9f3367da4a43404609cc9805f617a17
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-04-12 12:31:00 +0500

    Dars-3

---
+++ Dars-3/index.js
@@ -0,0 +1,18 @@
+// let data = 9
+// if(data<10){
+// console.log("Havo sovuq");
+// }else if(data<20){
+//     console.log("Havo iliq");
+// }else if (data<30){
+//     console.log("Havo issiq");
+// }else{
+//     console.log("Bunaqa obhavo mavjud emas");
+// }
+// data<10?console.log("To'gri"): console.log("Noto'gri");
+// console.log(true|| false);
+// console.log(12 && 1)
+console.log(false ?? 2);// undefined null kelmasa bo'ldi
+
+
+
+
