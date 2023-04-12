8b3147dc7161d3fdf0776d72e1c83e4c32a3793a
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-04-12 16:36:31 +0500

    switch

--- Dars-4/index.js
+++ Dars-4/index.js
@@ -97,156 +97,54 @@
 //     }
 // }
 // console.log(res);
-let num = 856
-let yuzlik = parseInt(num / 100)
-num = num % 100
+// let num = 856
+// let yuzlik = parseInt(num / 100)
+// num = num % 100
 
-let onlik = parseInt(num / 10)
-num = num % 10
+// let onlik = parseInt(num / 10)
+// num = num % 10
 
-let birlik = num
-a = "", b = "", c = "";
-switch (yuzlik) {
-    case 1: a = "Bir yuz"; break;
-    case 2: a = "Ikki yuz"; break;
-    case 3: a = "Uch yuz"; break;
-    case 4: a = "To'rt yuz"; break;
-    case 5: a = "Besh yuz"; break;
-    case 6: a = "Olti yuz"; break;
-    case 7: a = "Yetti yuz"; break;
-    case 8: a = "Sakkiz yuz"; break;
-    case 9: a = "To'qqiz yuz"; break;
-}
-switch (onlik) {
-    case 1: b = "o'n"; break;
-    case 2: b = "yigirma"; break;
-    case 3: b = "o'ttiz"; break;
-    case 4: b = "qirq"; break;
-    case 5: b = "ellik"; break;
-    case 6: b = "oltmish"; break;
-    case 7: b = "yetmish"; break;
-    case 8: b = "sakson"; break;
-    case 9: b = "to'qson"; break;
-}
-switch (birlik) {
-    case 1: c = "Bir"; break;
-    case 2: c = "Ikki"; break;
-    case 3: c = "Uch"; break;
-    case 4: c = "To'rt"; break;
-    case 5: c = "Besh"; break;
-    case 6: c = "Olti"; break;
-    case 7: c = "Yetti"; break;
-    case 8: c = "Sakkiz"; break;
-    case 9: c = "To'qqiz"; break;
-}
-
-console.log(a, b, c);
-
-// let num =123
-
-// let yuzlig = parseInt(num / 100);
-// num = num % 100;
-
-// let onlig = parseInt(num / 10);
-// num = num % 10;
-
-// let birlig = num;
+// let birlik = num
 // a = "", b = "", c = "";
-
-// switch (yuzlig) {
-//     case 1:
-//         a = "bir yuz";
-//         break;
-//     case 2:
-//         a = "ikki yuz";
-//         break;
-//     case 3:
-//         a = "uch yuz";
-//         break;
-//     case 4:
-//         a = "to'r yuz";
-//         break;
-//     case 5:
-//         a = "bech yuz";
-//         break;
-//     case 6:
-//         a = "olti yuz";
-//         break;
-//     case 7:
-//         a = "yetti yuz";
-//         break;
-//     case 8:
-//         a = "sakkiz yuz";
-//         break;
-//     case 9:
-//         a = "to'qqiz yuz";
-//         break;
+// switch (yuzlik) {
+//     case 1: a = "Bir yuz"; break;
+//     case 2: a = "Ikki yuz"; break;
+//     case 3: a = "Uch yuz"; break;
+//     case 4: a = "To'rt yuz"; break;
+//     case 5: a = "Besh yuz"; break;
+//     case 6: a = "Olti yuz"; break;
+//     case 7: a = "Yetti yuz"; break;
+//     case 8: a = "Sakkiz yuz"; break;
+//     case 9: a = "To'qqiz yuz"; break;
 // }
-// switch (onlig) {
-//     case 1:
-//         b = "o'n";
-//         break;
-//     case 2:
-//         b = "yigirma";
-//         break;
-//     case 3:
-//         b = "o'ttiz";
-//         break;
-//     case 4:
-//         b = "qiriq";
-//         break;
-//     case 5:
-//         b = "ellig";
-//         break;
-//     case 6:
-//         b = "oltmush";
-//         break;
-//     case 7:
-//         b = "yetmush";
-//         break;
-//     case 8:
-//         b = "sakson";
-//         break;
-//     case 9:
-//         b = "to'qson";
-//         break;
+// switch (onlik) {
+//     case 1: b = "o'n"; break;
+//     case 2: b = "yigirma"; break;
+//     case 3: b = "o'ttiz"; break;
+//     case 4: b = "qirq"; break;
+//     case 5: b = "ellik"; break;
+//     case 6: b = "oltmish"; break;
+//     case 7: b = "yetmish"; break;
+//     case 8: b = "sakson"; break;
+//     case 9: b = "to'qson"; break;
 // }
-// switch (birlig) {
-//     case 1:
-//         c = "bir";
-//         break;
-//     case 2:
-//         c = "ikki";
-//         break;
-//     case 3:
-//         c = "uch";
-//         break;
-//     case 4:
-//         c = "to'rt";
-//         break;
-//     case 5:
-//         c = "bech";
-//         break;
-//     case 6:
-//         c = "olti";
-//         break;
-//     case 7:
-//         c = "yetti";
-//         break;
-//     case 8:
-//         c = "sakkiz";
-//         break;
-//     case 9:
-//         c = "to'qqiz";
-//         break;
+// switch (birlik) {
+//     case 1: c = "Bir"; break;
+//     case 2: c = "Ikki"; break;
+//     case 3: c = "Uch"; break;
+//     case 4: c = "To'rt"; break;
+//     case 5: c = "Besh"; break;
+//     case 6: c = "Olti"; break;
+//     case 7: c = "Yetti"; break;
+//     case 8: c = "Sakkiz"; break;
+//     case 9: c = "To'qqiz"; break;
 // }
-// console.log(a, b,c);
 
-
-
-
-
-
-
-
-
+// console.log(a, b, c);
+let num = 122
+let str = num.toString()
+let res = 0
+for (i = 0; i < str.length; i++) {
+    res += +str[i]
+}
+console.log(res);
