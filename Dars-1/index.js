a3d0359de2241951ccb9be93da400f0f21e32da0
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-04-12 12:32:55 +0500

    Dars-1

---
+++ Dars-1/index.js
@@ -0,0 +1,46 @@
+// let 
+// var 
+// const
+// let str = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus ea perspiciatis explicabo ratione impedit ab culpa voluptate voluptatibus delectus voluptatum ducimus modi laboriosam odio amet animi illo unde accusantium officiis eligendi laudantium reprehenderit praesentium dolorum, sed distinctio. Rerum, itaque consequuntur molestiae quia consectetur a qui accusantium vero tempore quas aperiam!"
+// var str1 = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus ea perspiciatis explicabo ratione impedit ab culpa voluptate voluptatibus delectus voluptatum ducimus modi laboriosam odio amet animi illo unde accusantium officiis eligendi laudantium reprehenderit praesentium dolorum, sed distinctio. Rerum, itaque consequuntur molestiae quia consectetur a qui accusantium vero tempore quas aperiam!"
+// str="salom"
+// str1="hello js"
+// const num =123456789
+// num=987654321
+// console.log(num);
+// console.log("webdasturlash 1")
+// console.log("webdasturlash 2")
+// console.log("webdasturlash 3")
+// let num = 2
+// let a = 12
+// let b = 15
+// c= 1
+// console.log(num + a + b+c);
+// let num
+// console.log(num);
+// let str = "123456" 
+// //                             Number 
+// let num = '12.6n56'
+// console.log(Number(num));
+// console.log(Number.parseInt(num));
+// console.log(Number.parseFloat(num));
+// let num =9007199254740992n+8n// BigInt
+// console.log(num);
+// Boolean
+// true  //  ha 
+// false // yoq
+// let str = undefined
+// let str = Symbol
+// let str1 = Symbol
+// console.log(str == str1)
+// null 
+// let str ="webdasturlash"
+// let str1 ='webdasturlash'
+// let str2= `webdasturlash`
+// console.log(str,str1,str2);
+// let obj ={
+//     name:"webdastirlash",
+//     surname:"WEB",
+//     age:18
+// }
+// console.log(obj);
