073b8506463bf395e1290c707df7abab6d2859c9
Muhammadjonmurodullayev <Muhammadjonmurodullayev2005@gmail.com>
2023-04-14 11:50:38 +0500

    while,do while

---
+++ Dars-5/index.js
@@ -0,0 +1,20 @@
+// let str = "webbrain"
+// for (let i = 1; i <= 2; i++) {
+//     console.log(i);
+//     for (let j = 1 ; j <= 3; j++) {
+//         console.log(j);
+//     }
+
+// }
+// continue----bizaga qaysi sonzi continu qilsak o'sha belgilagan malumotimizga kelganda oshani oqimiydida boshiga qaytib qoladi yani o'sh bergan malumotimizni tushuib qoldiradi 
+// break----------bu bizaga qaysi malumotni break qilsak o'sha malumotga  kelganda to'xtab qoladi
+// let num = "webdasturlash"
+// for(let i=0;i<num.length;i++){
+//     if(num[i]=="d"){
+//         continue;
+//     }
+    
+//   else{
+//     console.log(num[i]);
+//   }
+// }
